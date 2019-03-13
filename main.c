4665c194d3ecfc0fcaaaa89e6b80003f152193c2
Pedro Esteves <up201705160@fe.up.pt>
2019-03-13 16:34:21 +0000

    Update .gitignore

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
-//Main program that will be called from the terminal
+/* 
+    Main program that will be called from the terminal
+*/
+
 #include <stdio.h>
 
 int main(int argc, char* argv[], char* envp[]){
     if(argc < 2) {
-         printf("Error: program needs at least 1 parameter...\n");
+        printf("Error: program needs at least 1 parameter...\n");
         return 1;
     }
 
