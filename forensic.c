82ea3c0f89d837120196ceea0f7cfb15404255b9
Pedro Esteves <up201705160@fe.up.pt>
2019-03-23 18:42:49 +0000

    Extracting and printing file information

--- forensic.c
+++ forensic.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "forensic.h"
 
@@ -22,41 +26,116 @@ int main(int argc, char *argv[], char *envp[])
         return 1;
     }
 
-    fore_args args = get_programs_to_execute(argc, argv);
+    fore_args args = get_programs_to_execute(argc, argv, envp);
 
-    char *logfilename;
-    if (args.arg_v)
-        logfilename = get_filename_var(envp);
-    else
-        logfilename = NULL;
+    //Read File Type
+    int fd1 = open("temp_file.txt", O_RDWR, 0777);
+    FILE *fp = fdopen(fd1, "r");
+    char *file_string = malloc(255 * sizeof(char));
+    sprintf(file_string, "file %s > temp_file.txt", args.f_or_dir);
+    system(file_string);
+    free(file_string);
+    file_string = malloc(255 * sizeof(char));
+    fgets(file_string, 255, fp);
+    file_string = strstr(file_string, " ") + 1;
+    fclose(fp);
+    close(fd1);
 
-    free(logfilename);
+    file_string[strcspn(file_string, "\n")] = '\0';
+
+    //Read File Data
+    fd1 = open("temp_file.txt", O_RDWR, 0777);
+    fp = fdopen(fd1, "r");
+    char *lsl_string = malloc(255 * sizeof(char));
+    sprintf(lsl_string, "ls -l %s > temp_file.txt", args.f_or_dir);
+    system(lsl_string);
+    free(lsl_string);
+    lsl_string = malloc(255 * sizeof(char));
+    fgets(lsl_string, 255, fp); //Get ls -l string from file
+
+    char *file_access_owner = malloc(3 * sizeof(char));
+
+    for (unsigned i = 0, j = 0; i < 3; i++, j++)
+    {
+        if (lsl_string[i] == '-')
+        {
+            j--;
+            continue;
+        }
+        file_access_owner[j] = lsl_string[i];
+    }
+    lsl_string = strstr(lsl_string, " ") + 1;
+    lsl_string = strstr(lsl_string, " ") + 1;
+    lsl_string = strstr(lsl_string, " ") + 1;
+    lsl_string = strstr(lsl_string, " ") + 1;
+
+    int file_size = 0;
+    sscanf(lsl_string, "%d", &file_size);
+    lsl_string = strstr(lsl_string, " ") + 1;
+
+    char *file_modification_date = malloc(12 * sizeof(char));
+    for (unsigned i = 0; i < 12; i++)
+    {
+        file_modification_date[i] = lsl_string[i];
+    }
+
+    fclose(fp);
+    close(fd1);
+
+    printf("%s,%s,%d,%s,%s", args.f_or_dir, file_string, file_size, file_access_owner, file_modification_date);
+
+    //Calculate file fingerprints
+    if (args.arg_h)
+    {
+        unsigned i = 0;
+        while (args.h_args[i] != NULL || i < 3)
+        {
+            fd1 = open("temp_file.txt", O_RDWR, 0777);
+            fp = fdopen(fd1, "r");
+            char *h_string = malloc(255 * sizeof(char));
+            char* tmp_string = malloc(25*sizeof(char));
+            sprintf(h_string, "%ssum %s > temp_file.txt", args.h_args[i], args.f_or_dir);
+            system(h_string);
+            h_string = malloc(255 * sizeof(char));
+            fgets(h_string, 255, fp);
+            sscanf(h_string, "%s %s", h_string, tmp_string);
+            printf(",%s", h_string);
+            free(h_string);
+            free(tmp_string);
+            fclose(fp);
+            close(fd1);
+            i++;
+        }
+    }
+    printf("\n");
     return 0;
 }
 
-fore_args get_programs_to_execute(int argc, char *argv[])
+fore_args get_programs_to_execute(int argc, char *argv[], char *envp[])
 {
     fore_args args;
     args.arg_r = false;
     args.arg_h = false;
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++)
+    {
         args.h_args[i] = NULL;
     }
     args.arg_o = false;
     args.outfile = NULL;
     args.arg_v = false;
+    args.logfilename = NULL;
     args.f_or_dir = NULL;
 
     for (int i = 1; i < argc - 1; i++)
     {
         if (strcmp(argv[i], "-r") == 0)
         {
-            args.arg_r = 1;
+            args.arg_r = true;
             continue;
         }
         else if (strcmp(argv[i], "-h") == 0)
         {
-            args.arg_h = 1;
+            args.arg_h = true;
             i++;
 
             char *h_arg = (char *)malloc(20);
@@ -131,32 +210,20 @@ fore_args get_programs_to_execute(int argc, char *argv[])
         }
         else if (strcmp(argv[i], "-o") == 0)
         {
-            args.arg_o = 1;
+            args.arg_o = true;
             i++;
             args.outfile = argv[i];
             continue;
         }
         else if (strcmp(argv[i], "-v") == 0)
         {
-            args.arg_v = 1;
+            args.arg_v = true;
+            args.logfilename = get_filename_var(envp);
             continue;
         }
     }
 
     args.f_or_dir = argv[argc - 1];
-
-    /*printf("%d\n", args.arg_r);
-    printf("%d\n", args.arg_h);
-    if (args.h_args[0] != NULL)
-        printf("%s\n", args.h_args[0]);
-    if (args.h_args[1] != NULL)
-        printf("%s\n", args.h_args[1]);
-    if (args.h_args[2] != NULL)
-        printf("%s\n", args.h_args[2]);
-    printf("%d\n", args.arg_o);
-    printf("%s\n", args.outfile);
-    printf("%d\n", args.arg_v);
-    printf("%s\n", args.f_or_dir);*/
 
     return args;
 }
--- forensic.h
+++ forensic.h
@@ -2,15 +2,18 @@
 
 typedef struct
 {
-    bool arg_r;      //Option -r of forensic program
-    bool arg_h;      //Option -h of forensic program
-    char *h_args[3]; //Option -h may have 3 arguments
-    bool arg_o;      //Option -o of forensic program
-    char *outfile;   //If -o has an argument, it will be stored here
-    bool arg_v;      //Option -v of forensic program
-    char *f_or_dir;  //File or directory to be evaluated
+    bool arg_r;        //Option -r of forensic program
+    bool arg_h;        //Option -h of forensic program
+    char *h_args[3];   //Option -h may have 3 arguments
+    bool arg_o;        //Option -o of forensic program
+    char *outfile;     //If -o has an argument, it will be stored here
+    bool arg_v;        //Option -v of forensic program
+    char* logfilename; //If -v is selected, this stores the value of LOGFILENAME variable
+    char *f_or_dir;    //File or directory to be evaluated
 } fore_args;
 
-fore_args get_programs_to_execute(int argc, char* argv[]);
+fore_args get_programs_to_execute(int argc, char* argv[], char* envp[]);
 
 char* get_filename_var(char* envp[]);
+
+void remove_newline(char* string);
