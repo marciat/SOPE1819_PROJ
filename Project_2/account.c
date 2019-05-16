c5cae52687bc09764c3d2079ccf8ef46aa6f47b9
Joao Leite <jopamoleite@gmail.com>
2019-05-16 12:11:15 +0100

    Fixed operation functions, autentication and replies

--- Project_2/account.c
+++ Project_2/account.c
@@ -32,17 +32,22 @@ ret_code_t create_client_account(req_value_t* client_information, int thread_id,
 	reply->length = sizeof(rep_header_t);
 	reply->value.header.account_id = client_information->header.account_id;
 	
-	if(client_information->header.account_id == 0 || accounts[client_information->header.account_id].account_id != 0){
+	if(client_information->header.account_id != 0){
+		return RC_OP_NALLOW;
+	}
+
+	if(client_information->create.account_id == 0 || accounts[client_information->create.account_id].account_id != 0){
 		return RC_ID_IN_USE;
 	}
 
 	bank_account_t account;
 
-	account.account_id = client_information->header.account_id;
+	account.account_id = client_information->create.account_id;
 	salt_generator(account.salt);
-	get_hash(client_information->header.password, account.salt, account.hash);
+	get_hash(client_information->create.password, account.salt, account.hash);
 
-	account.balance = 0;
+	account.balance = client_information->create.balance;
+	printf("Balance: %d\n", client_information->create.balance);
 
 	accounts[account.account_id] = account;
 
@@ -123,7 +128,7 @@ ret_code_t money_transfer(uint32_t account_id, char* password, uint32_t new_acco
 	}
 
 	char new_hash[HASH_LEN];
-	get_hash(password, accounts[account_id].hash, new_hash);
+	get_hash(password, accounts[account_id].salt, new_hash);
 
 	if(strcmp(new_hash, accounts[account_id].hash) != 0){
 		if(logSyncMechSem(server_logfile, pthread_self(), SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_CONSUMER, 0, 0) < 0){
@@ -210,6 +215,7 @@ ret_code_t check_balance(uint32_t account_id, char* password, tlv_reply_t *reply
 			perror("pthread_mutex_unlock");
 			exit(-1);
 		}
+		reply->value.balance.balance = 0;
 		return RC_OP_NALLOW;
 	}
 
@@ -221,11 +227,11 @@ ret_code_t check_balance(uint32_t account_id, char* password, tlv_reply_t *reply
 			perror("pthread_mutex_unlock");
 			exit(-1);
 		}
+		reply->value.balance.balance = 0;
 		return RC_ID_NOT_FOUND;	
 	}
-
 	char new_hash[HASH_LEN];
-	get_hash(password, accounts[account_id].hash, new_hash);
+	get_hash(password, accounts[account_id].salt, new_hash);
 
 	if(strcmp(new_hash, accounts[account_id].hash) != 0){
 		if(logSyncMechSem(server_logfile, pthread_self(), SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_CONSUMER, 0, 0) < 0){
@@ -235,10 +241,9 @@ ret_code_t check_balance(uint32_t account_id, char* password, tlv_reply_t *reply
 			perror("pthread_mutex_unlock");
 			exit(-1);
 		}
+		reply->value.balance.balance = 0;
 		return RC_LOGIN_FAIL;
 	}
-
-	printf("%d\n", accounts[account_id].balance);
 
 	if(logSyncMechSem(server_logfile, pthread_self(), SYNC_OP_MUTEX_UNLOCK, SYNC_ROLE_CONSUMER, 0, 0) < 0){
 		printf("Log sync mech sum error!\n");
@@ -247,6 +252,8 @@ ret_code_t check_balance(uint32_t account_id, char* password, tlv_reply_t *reply
 		perror("pthread_mutex_unlock");
 		exit(-1);
 	}
+
+	reply->value.balance.balance = accounts[account_id].balance;
 
 	return RC_OK;
 }
@@ -311,4 +318,6 @@ void get_hash(char* password, char* salt, char* hash){
 		while(read(pipe_fd[READ], hash, HASH_LEN) == 0);
 		close(pipe_fd[READ]);
 	}
+
+	hash[HASH_LEN] = '\0';
 }
